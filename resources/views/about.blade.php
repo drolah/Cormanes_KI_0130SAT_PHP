c11cb4db84e566783af8b866bdc9f2a184bac433
drolah <akiraisabelle12@gmail.com>
2024-09-22 21:33:51 +0800

    0914_4_make-a-pretty-layout-using-tailwindcss

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        About Page
+    </x-slot:heading>
+
     <h1>Hello, im jule brian</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,7 @@
 <x-layout>
+    <x-slot:heading>
+        Contact Page
+    </x-slot:heading>
+
     <h1>Hello, contact this #091234567</h1>
 </x-layout>
