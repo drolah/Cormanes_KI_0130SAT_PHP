7191f083d3e11a5cb8a0fb5b8e4809e551747955
drolah <akiraisabelle12@gmail.com>
2024-10-03 21:38:09 +0800

    0921_17_PDO_first_steps

--- index.php
+++ index.php
@@ -2,4 +2,13 @@
 
 require 'functions.php';
 
-require 'router.php';
+$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("select * from posts where id = 1");
+$statement->execute();
+
+$post = $statement->fetch(PDO::FETCH_ASSOC);
+
+echo "<li>" . $post['title'] . "</li>";
