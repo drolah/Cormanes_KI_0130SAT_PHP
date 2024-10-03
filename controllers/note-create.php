58c692c3dc98aabdb1fcb7f854087773ea2c33d7
drolah <akiraisabelle12@gmail.com>
2024-10-03 10:58:24 +0800

    0928_26_always_escape_untrusted_input

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -3,7 +3,10 @@
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Next episode: Create new note using form data from $_POST.
+    $db->query('INSERT INTO notes(body, user_id) VALUES(:body, :user_id)', [
+        'body' => $_POST['body'],
+        'user_id' => 1
+    ]);
 }
 
 require 'views/note-create.view.php';
