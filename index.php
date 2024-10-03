674e9dff45aac1c2eacccd5dbf197ad63121da4f
drolah <akiraisabelle12@gmail.com>
2024-10-03 21:44:06 +0800

    0921_19_environments_and_configurations_flexibility

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 require 'functions.php';
 require 'Database.php';
 
-$db = new Database();
-$posts = $db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
+$config = require('config.php');
+$db = new Database($config['database']);
+$posts = $db->query("select * from posts")->fetchAll();
 
 dd($posts);
